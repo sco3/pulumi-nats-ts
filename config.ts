d9400d8af485e6fa5f06507c0e5e75b179dff03f
Dmitry Zakharov <zakharov@ie.ibm.com>
2025-09-02 10:10:41 +0100

    stop & single node

--- config.ts
+++ config.ts
@@ -1,4 +1,4 @@
-export const numReplicas = 3;
+export const numReplicas = 1;
 export const appName = "nats";
 export const natsImage = "docker.io/nats:2.11.8-alpine3.22";
 export const networkName = "nats-net";
